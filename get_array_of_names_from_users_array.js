355482c40747d0b33a9348c49c44219af4dfe9af
amr_mohie <damrmohy@gmail.com>
2023-03-29 06:32:57 +0200

    Update get_array_of_names_from_users_array.js
    sort,get by active

--- get_array_of_names_from_users_array.js
+++ get_array_of_names_from_users_array.js
@@ -1,9 +1,19 @@
 const users = [
-    {id: 1, name: 'amr', isActive: true},
-    {id: 2, name: 'alex', isActive: false},
-    {id: 3, name: 'momo', isActive: true},
-    {id: 4, name: 'dev', isActive: false},
+    {id: 1, name: 'amr', isActive: true, age: 27},
+    {id: 2, name: 'alex', isActive: false, age: 22},
+    {id: 3, name: 'momo', isActive: true, age: 20},
+    {id: 4, name: 'dev', isActive: false, age: 30},
 ];
 
+// all names
 const names = users.map(user => user.name);
 console.log(names);
+
+// get only active users
+const active_users_names = users
+    .filter(user => user.isActive)
+    .map(user => user.name);
+console.log(active_users_names);
+
+const sorted_users = users
+    .sort((user1,user2) => (user1.age < user2.age ? 1 :-1));
